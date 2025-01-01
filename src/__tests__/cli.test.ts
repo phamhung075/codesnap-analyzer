85698b16e44637544d31f9d509f7842e6f1e0288
Dai Hung PHAM <daihung.pham@gmail.com>
2025-01-01 11:55:37 +0100

    fix test

--- src/__tests__/cli.test.ts
+++ src/__tests__/cli.test.ts
@@ -1,6 +1,3 @@
-// src/__tests__/cli.test.ts
-import { describe, it, expect } from '@jest/globals';
-
 describe('Sample Test', () => {
   it('should pass this test', () => {
     expect(true).toBe(true);
