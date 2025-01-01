872a80f5527a924d6948c340d6d4cb8b4e818fcb
Dai Hung PHAM <daihung.pham@gmail.com>
2025-01-01 11:52:38 +0100

    add simple test

---
+++ src/__tests__/cli.test.ts
@@ -0,0 +1,8 @@
+// src/__tests__/cli.test.ts
+import { describe, it, expect } from '@jest/globals';
+
+describe('Sample Test', () => {
+  it('should pass this test', () => {
+    expect(true).toBe(true);
+  });
+});
